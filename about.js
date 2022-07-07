df0fdb1e8e8d7c2472bdaee4549f4cede7a475ba
DanishCoder <Danishim66@gmail.com>
2022-07-07 14:19:38 -0400

    final commit

--- about.js
+++ about.js
@@ -1,5 +1,16 @@
 console.log("hello world");
 
+let cat = document.querySelector('#cat')
+
+const catMessage = () => {
+	alert('The cat is cute right')
+
+}
+
+cat.addEventListener('mouseover', catMessage)
+
+
+
 function handleCat (event) {
 	event.addEventListener();
 	alert("You have a great brain");
---
+++ profile.js
@@ -0,0 +1,29 @@
+
+
+// favorite color
+let colorBtn = document.querySelector('#color')
+
+const favoriteColor = () => {
+    alert('Blue')
+}
+
+colorBtn.addEventListener('click', favoriteColor)
+
+//my favorite place
+let placeBtn = document.querySelector('#place')
+
+const favoritePlace = () => {
+    alert('Egypt')
+}
+
+placeBtn.addEventListener('click', favoritePlace)
+
+// my favorite ritual 
+
+let ritualBtn = document.querySelector('#ritual')
+
+const favoriteRitual = () => {
+    alert('rain dance')
+}
+
+ritualBtn.addEventListener('click', favoriteRitual)
